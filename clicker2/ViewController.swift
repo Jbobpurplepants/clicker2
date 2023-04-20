7e5d2de263899298de6a595b628a17df54ce505d
Jacob Phipps <houseofphipps@gmail.com>
2023-04-20 10:17:42 -0600

    started the app

--- clicker2/ViewController.swift
+++ clicker2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var clicksLabel: UILabel!
+    
+    @IBOutlet weak var clickButton: UIButton!
+    
+    @IBOutlet weak var buyClickerButton: UIButton!
+    
+    @IBOutlet weak var buyClickerLabel: UILabel!
+    var clicks = 0.0 {
+        didSet {
+            clicksLabel.text = "Clicks: \(clicks)"
+        }
+    }
+    
+    var clicker = 10.0 {
+        didSet {
+            buyClickerLabel.text = "Buy Clicker: \(clicker)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clickButton.layer.cornerRadius = 16
     }
-
-
+    
+    @IBAction func clickButtonPressed(_ sender: Any) {
+        clicks = clicks + 1.0
+        
+    }
+    @IBAction func buyClickerButtonPressed(_ sender: Any) {
+        if clicks < clicker {
+            return
+        }
+        if clicks >= clicker {
+            clicks -= clicker
+            clicker = clicker * 1.2
+            clicks = clicks + 1.0
+            
+        }
+        
+    }
+    
 }
-

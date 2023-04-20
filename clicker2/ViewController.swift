bd9c1268346329b3eff819fa13d996528f6f39f0
Jacob Phipps <houseofphipps@gmail.com>
2023-04-20 09:48:40 -0600

    Initial Commit

---
+++ clicker2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clicker2
+//
+//  Created by Phipps, Jacob - Student on 4/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
